7edd313e9080f8f7341bb385fffbf30763a4e146
kudane <kraisorn204@gmail.com>
2021-05-24 15:39:29 +0700

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { combineLatest } from 'rxjs';
+import { combineLatest, Subscription } from 'rxjs';
 import { map, startWith, switchMap, tap } from 'rxjs/operators';
 import { MockDataService } from './mock-data.service';
 import { PeriodicItem } from './mock.model';
 
 class TableControl {
+  private _subscriptions$: Subscription;
   // for get data
   private _dataService: MockDataService;
 
@@ -14,14 +21,14 @@ class TableControl {
   private _textControl = new FormControl('');
 
   // for table
-  public readonly columns: string[] = ['position', 'name', 'weight', 'symbol'];
+  public readonly columns = ['position', 'name', 'weight', 'symbol'];
   private _paginator: MatPaginator;
   private _totalCount: number = 0;
   private _dataSource: PeriodicItem[] = [];
-  get data() {
+  public get data() {
     return this._dataSource;
   }
-  get totalCount() {
+  public get totalCount() {
     return this._totalCount;
   }
 
@@ -31,18 +38,24 @@ class TableControl {
   }
 
   public watchSearch() {
-    // add listen stram of interactive with table
-    combineLatest([
-      // init value for init starm
+    if (!this._paginator) {
+      throw new Error('paginator is null, please set in ngAfterViewInit');
+    }
+
+    // Array ของ Observable เมื่อ search หรือ page เปลี่ยนแปลง
+    // เพิ่ม startWith สำหรับ initial data หรือ ทำให้เกิดการ call service ครั้งแรก เพราะ
+    // combineLatest จะปล่อยค่าออกมา เมื่อ Observable ใน Array มีการปล่อยค่าออกมาทุกตัว
+    this._subscriptions$ = combineLatest([
       this._paginator.page.pipe(
         startWith({ length: 0, pageIndex: 0, pageSize: 2 } as PageEvent)
       ),
       this._textControl.valueChanges.pipe(startWith(''))
     ])
       .pipe(
-        // log data
+        // log ดูข้อมูล
         tap(([page, search]) => console.log(page, search)),
-        // stop before stram, then start stram in [switchMap]
+        // unsubscribe ที่ combineLatest แล้ว
+        // เปลี่ยนไป get data จาก service
         switchMap(() => {
           return this._dataService.getItemsWithPagination(
             this._textControl.value,
@@ -50,22 +63,26 @@ class TableControl {
             this._paginator.pageIndex
           );
         }),
-        // get data for update count of table row
+        // อัพเดท total count และ ปล่อยค่าเฉพาะ items
         map(({ items, totalCount }) => {
           this._totalCount = totalCount;
           return items;
         })
       )
-      .subscribe(items => {
-        // store table row & displayed
-        this._dataSource = items;
-      });
+      .subscribe((items: PeriodicItem[]) => (this._dataSource = items));
   }
 
-  search(text: string) {
+  public search(text: string) {
     // reset to first page
     this._paginator.pageIndex = 0;
     this._textControl.setValue(text);
+  }
+
+  public destroy() {
+    this._subscriptions$.unsubscribe();
+    this._dataSource = null;
+    this._paginator = null;
+    this._textControl = null;
   }
 }
 
@@ -74,7 +91,7 @@ class TableControl {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnDestroy {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   tableControl: TableControl;
 
@@ -83,5 +100,9 @@ export class AppComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.tableControl = new TableControl(this.dataService, this.paginator);
     this.tableControl.watchSearch();
+  }
+
+  ngOnDestroy(): void {
+    this.tableControl.destroy();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material.module';
 import { SharedModule } from './shared.module';
 
 import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   imports: [
